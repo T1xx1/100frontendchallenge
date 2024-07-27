269de9d41786a242009a52fed3ff913856bba1e7
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-07-27 21:32:08 +0200

    Colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            orange: '#ff8000',
+            blue: '#00f',
+
+            /*  */
+
+            primary: '#00f', /* orange */
+            accent: '#ff8000'     /* blue */
+         }
+      },
    },
    plugins: [],
 };
