b3f33bde905e3efac80d303e2f7fe24742213223
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-08-04 22:06:31 +0200

    Image carousel challenge

---
+++ src/content/components/imgcarousel/carousel.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+import img1 from './img1.jpg';
+import img2 from './img2.jpg';
+import img3 from './img3.jpg';
+import img4 from './img4.jpg';
+import img5 from './img5.jpg';
+import img6 from './img6.jpg';
+import img7 from './img7.jpg';
+import left from './left.svg';
+import right from './right.svg';
+
+const imgs = [
+   img1,
+   img2,
+   img3,
+   img4,
+   img5,
+   img6,
+   img7
+];
+
+export default function Carousel() {
+   const [index, setIndex] = useState(0);
+
+   const scroll = (n: number) => {
+      const newIndex = index + n;
+
+      if (newIndex === -1) {
+         setIndex(imgs.length - 1);
+      } else if (newIndex === imgs.length) {
+         setIndex(0);
+      } else {
+         setIndex(newIndex);
+      }
+   };
+
+   return <div className='space-y-3'>
+      <div className='w-full flex gap-3 items-center'>
+         <button type='button' onClick={() => scroll(-1)}>
+            <img src={left.src} alt='Previous' className='hover:bg-gray-700 bg-opacity-50 rounded-md p-1 h-8 w-8' />
+         </button>
+
+         <img src={imgs[index].src} alt='Image' className='rounded-lg aspect-video h-72 object-cover' />
+
+         <button type='button' onClick={() => scroll(+1)}>
+            <img src={right.src} alt='Next' className='hover:bg-gray-700 bg-opacity-50 rounded-md p-1 h-8 w-8' />
+         </button>
+      </div>
+
+      <div className='flex gap-3 justify-center'>
+         {imgs.map((img, i) => (
+            <button type='button' onClick={() => setIndex(i)}>
+               <img src={img.src} alt='Img' className={`rounded-lg h-16 w-16 object-cover ${i === index && 'outline outline-white'}`} />
+            </button>
+         ))}
+      </div>
+   </div>;
+}
