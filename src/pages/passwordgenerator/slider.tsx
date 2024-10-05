f1c7ad849178d614f7819f488877a203299e3f99
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-10-05 22:21:27 +0200

    Change: passwordgenerator ui

--- src/pages/passwordgenerator/slider.tsx
+++ src/pages/passwordgenerator/slider.tsx
@@ -15,10 +15,10 @@ const Slider = React.forwardRef<
       )}
       {...props}
    >
-      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-100 dark:bg-slate-800">
-         <SliderPrimitive.Range className="absolute h-full bg-slate-900 dark:bg-slate-50" />
+      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-400 dark:bg-slate-800">
+         <SliderPrimitive.Range className="absolute h-full bg-violet-500 dark:bg-slate-50" />
       </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-900 bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-slate-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300" />
+      <SliderPrimitive.Thumb className="block h-6 w-6 rounded-full border-4 border-violet-500 bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-slate-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300" />
    </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
